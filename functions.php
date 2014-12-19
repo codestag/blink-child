bfc3edb2bb1d6e437c7baba25899bc0180a8db91
Ram Ratan Maurya <mauryaratan@users.noreply.github.com>
2014-12-19 12:35:59 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,6 +8,6 @@
  */
 
 function blink_child_styles() {
-    wp_enqueue_style( 'blink-child', get_stylesheet_uri() );
+    wp_enqueue_style( 'blink-parent-theme', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'blink_child_styles', 999 );
+add_action( 'wp_enqueue_scripts', 'blink_child_styles' );
