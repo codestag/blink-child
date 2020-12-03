68c87e00d57a25dc1e18c637d660ed4a73363cb4
Krishna Kant <lushkant@gmail.com>
2020-12-03 16:14:02 +0530

    Update files

--- functions.php
+++ functions.php
@@ -7,7 +7,14 @@
  * @since Blink Child 1.0
  */
 
+/**
+ * Include child theme styles.
+ *
+ * @return void
+ */
 function blink_child_styles() {
-    wp_enqueue_style( 'blink-parent-theme', get_template_directory_uri() . '/style.css' );
+	$theme = wp_get_theme();
+	wp_enqueue_style( 'blink-style', get_template_directory_uri() . '/style.css', array(), $theme->parent()->get( 'Version' ) );
+	wp_enqueue_style( 'blink-child-style', get_stylesheet_uri(), array( 'blink-style' ), $theme->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'blink_child_styles' );
